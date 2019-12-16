3e5913c91d6bc8336207d46d9762bf3b1ed1c410
kriztpow <45737457+kriztpow@users.noreply.github.com>
2019-12-16 01:50:19 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ def main():
 
     print(data)  # Comment to hide what Telegram is sending you
     chat_id = data['message']['chat']['id']
-    message = data['message']['text']
+    message = data['text']
     hola = 'mamamelabolanegra'
     message_urld = BOT_URL + 'sendMessage' + '?text=Pija'
 
@@ -28,7 +28,7 @@ def main():
         "text": hola,
    }
     
-    message_url = BOT_URL + 'sendMessage' + '?text=Pija'
+    message_url = BOT_URL + 'sendMessage' + '?text=hola'
     requests.post(message_url, json=json_data)
     if message_url == message_urld:
         requests.post(message_url, json=json_datad)
