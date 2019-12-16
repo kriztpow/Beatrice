7107cc0d725a2638970dbb8790bea2a17e7750c7
kriztpow <45737457+kriztpow@users.noreply.github.com>
2019-12-16 04:12:43 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -51,13 +51,50 @@ def main():
     if message == llamarinter:
         requests.post(message_url, json=json_dataw)    
     else: 
-        requests.post(message_url, json=json_data) 
-        # Catch new_chat_member
+        requests.post(message_url, json=json_data)
+   #__________________________________
+  
+# Standard commands
+def start(bot, update):
+    bot.sendMessage(update.message.chat_id, text='Hi!')
+
+def help(bot, update):
+    bot.sendMessage(update.message.chat_id, text="Help!"
+
+                    
+def error(bot, update, error):
+    logger.warn('Update "%s" caused error "%s"' % (update, error))
+# Catch new_chat_member
 def welcome(bot,update):
     msg = update.message
     chat_id = msg.chat.id
-    bot.sendMessage(update.message.chat_id, "@%s \nWelcome!" % msg.new_chat_member.username)        
-    
+    bot.sendMessage(update.message.chat_id, "@%s \nWelcome!" % msg.new_chat_member.username)
+
+def main():
+    # Create the Updater and pass it your bot's token.
+    updater = Updater("<TOKEN>")
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+                    
+    # Message handlers
+    dp.add_handler(MessageHandler([Filters.status_update], welcome))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+                    
+    # Start the Bot
+    updater.start_polling()
+
+    updater.idle()
+
+   #__________________________    
     return ''
 
 if __name__ == '__main__':  
