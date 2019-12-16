02ffb6f698656cec704abf3f987c318cdbe2af64
kriztpow <45737457+kriztpow@users.noreply.github.com>
2019-12-15 23:06:52 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,19 +13,19 @@ def main():
     data = request.json
 
     print(data)  # Comment to hide what Telegram is sending you
-    chat_id = data['message']['chat']['id']['username']
+    chat_id = data['message']['chat']['id']
     message = data['message']['text']
-    inter = 'inter'
 
     json_data = {
         "chat_id": chat_id,
         "text": message,
     }
-    
+
     message_url = BOT_URL + 'sendMessage'
     requests.post(message_url, json=json_data)
 
     return ''
+
 
 if __name__ == '__main__':  
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
