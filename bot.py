2e42b339eb527472598eb27d74cec86d2a20ea96
kriztpow <45737457+kriztpow@users.noreply.github.com>
2019-12-17 00:12:25 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,8 @@ def main():
     print(data)  # Comment to hide what Telegram is sending you
     chat_id = data['message']['chat']['id']
     message = data['message']['text']
-    inter = '''Hola, para tu comercio, asegúrate de que el @InterBanex esté disponible, antes de tipear tus monedas.
+    user_id = data['first_name']
+    inter = '''Hola''' + user_id + ''', para tu comercio, asegúrate de que el @InterBanex esté disponible, antes de tipear tus monedas.
 
    ⚠️NOTA: Toda transacción tiene un costo del 1% del total de BANANO, NANO u otra moneda del @parjar_bot a comerciar, y ésta comisión debe pagarla el COMPRADOR en la moneda en cuestión.'''
     llamarinter = '/inter'
