4066a8a028d2a0b5d300e59b9828948204dd1f25
kriztpow <45737457+kriztpow@users.noreply.github.com>
2019-12-26 04:40:54 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,10 @@
 import requests  
 import os
 from flask import Flask, request
-import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-logger.setLevel(logging.DEBUG)
 
 BOT_URL = f'https://api.telegram.org/bot{os.environ["BOT_KEY"]}/'  # <-- add your telegram token as environment variable
 
 app = Flask(__name__)
-logger = logging.getLogger(__name__)
-
-def start(update, context):
-    update.message.reply_text(
-        "Hi! My name is Doctor Botter. I will hold a more complex conversation with you. "
-        "Why don't you tell me something about yourself?",
-        reply_markup=markup)
-
-    return CHOOSING
-
 
 @app.route('/', methods=['POST'])
 def main():  
